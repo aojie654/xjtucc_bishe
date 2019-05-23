c42d1341c2887db57dc9bc0ca15dae063c0f5122
Shengjyer.Ao <aojie654@live.cn>
2019-05-24 01:03:32 +0800

    Fixed bugs of 01_charset

--- python/01_charset.py
+++ python/01_charset.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from os import system as s
+from os import system as s, path as p
 from subprocess import getoutput as spgop
 import chardet
 
@@ -18,22 +18,33 @@ list_file_name_0 = find_result_0.split('\n')
 
 # 文件转码
 for list_file_name_0_t in list_file_name_0:
-    
+    # 获取当前文件所在路径
+    file_dir_0 = list_file_name_0_t[0:list_file_name_0_t.rfind('/')]
+
+    # 获取作品名
+    file_name_0 = file_dir_0[file_dir_0.rfind('/')+1:]
+
     # 文件源即为当前遍历元素
     file_source_name_0 = list_file_name_0_t
     print(file_source_name_0+", ", end="")
+    
+    # 判断是否已经存在转码后文件, 如果是, 跳过循环
+    if p.exists(file_dir_0+"/"+file_name_0+"_01_utf8.txt"):
+        print("[Skipped.]")
+        continue
 
     # 跳过别名和字典库,以及已经转换的文件
-    if ("alias.txt" in file_source_name_0) or ("dict.txt" in file_source_name_0) or ("_utf8.txt" in file_source_name_0):
+    if ("alias.txt" in file_source_name_0) or ("dict.txt" in file_source_name_0):
         print("[Skipped.]")
     else:
-        print("[Processing...]")
+        print("[Processing...]"+", encoding=", end="")
         try:
             # 以rb方式读取文件内容, 猜测编码
             open_file_source_rb_0 = open(file_source_name_0, 'rb')
             read_content_0 = open_file_source_rb_0.read()
             open_file_source_rb_0.close()
             read_charset_0 = chardet.detect(read_content_0)['encoding']
+            print(read_charset_0)
 
             # 以r方式和read_charset_0编码读取文件,忽略出错字符
             open_file_source_0 = open(file_source_name_0, 'r', encoding=read_charset_0, errors='ignore')
