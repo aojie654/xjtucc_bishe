4a5b6b840473562b5a83128f5c2f10a43ff03f68
aojie654 <aojie654@live.cn>
2020-01-12 19:39:08 +0800

    update 0_create_folder_and_move.py

--- tmp/xjtucc_bishe/python/0_create_folder_and_move.py
+++ tmp/xjtucc_bishe/python/0_create_folder_and_move.py
@@ -3,10 +3,27 @@
 import os
 import os.path as op
 
+# Save the system type
+system_type=os.uname().sysname
+
+# Init the filename list
 file_list = []
-os.chdir("D:\\tmp\\xjtucc_bishe\\create_folder")
+
+# Input the working folder
+working_folder = input("Inputs the working folder please:")
+
+# If system type is Windows, replace the '\' with '\\'
+if system_type == "Windows":
+    working_folder.replace("\\", "\\\\")
+
+# Change the working folder
+os.chdir(working_folder)
+#os.chdir("D:\\tmp\\xjtucc_bishe\\create_folder")
+
+# Walk in the working folder
 walk_result = os.walk(".")
 
+#
 for i in walk_result:
     file_list.append(i)
 
